a6e44f184b1ba0f12975a119399aff770050fc8a
spy <52524102@qq.com>
2022-11-09 10:51:39 +0800

    伤害计算（初级）

---
+++ calculate/calc.js
@@ -0,0 +1,18 @@
+function calc() {
+    const attack = +document.getElementById("attack").value;
+    const defend = +document.getElementById("defend").value;
+    const strike_val = +document.getElementById("strike_val").value;
+    const strike_def = +document.getElementById("strike_def").value;
+    const strike_mul = +document.getElementById("strike_mul").value / 100;
+    const d_strike_rate = +document.getElementById("d_strike_rate").value / 100;
+    const multiply = +document.getElementById("multiply").value / 100;
+    const d_damage_rate = +document.getElementById("d_damage_rate").value / 100;
+    const damage_eff = 2143 / (2143 + defend);
+    const f_strike_val = Math.max(strike_val - 0.6 * strike_def, 0);
+    const strike_rate = f_strike_val / (120 + 0.8 * f_strike_val);
+    const f_strike_rate = strike_rate + d_strike_rate;
+    const f_damage_rate = Math.max(1 + d_damage_rate, 0.3);
+    const damage = attack * multiply * damage_eff * f_damage_rate;
+    const damage_strike = damage * strike_mul;
+    document.getElementById("result").innerText = `造成伤害: ${damage.toFixed(2)}\n暴击率: ${(f_strike_rate * 100).toFixed(2)}%\n暴击伤害: ${damage_strike.toFixed(2)}`;
+}
